ce806db181248285eb0688b7dca1f4aec1830b60
Edward Jung <edwardjung2018@gmail.com>
2024-06-05 13:51:45 -0700

    finished clips routes, particularly edit

--- app/api/clip_routes.py
+++ app/api/clip_routes.py
@@ -40,54 +40,52 @@ def get_album_by_id(clip_id):
         clip_data['creator'] = clip.uploader.username
 
         # if the clip is private & the user logged in is not the creator of the clip
-        if clip_data['is_private']:
-            if clip.user_id != current_user.id:
-                response = jsonify({"error": "You are not authorized to view this clip"})
-                return response, 403
+        if clip_data['is_private'] and clip.user_id != current_user.id:
+            response = jsonify({"error": "You are not authorized to view this clip"})
+            return response, 403
 
         return clip_data
 
-    # if request.method in ["PUT", "DELETE"]:
-    #     if current_user.is_authenticated and album.artist_id == current_user.id:
-    #         pass
-    #     else:
-    #         return jsonify({"error": "Unauthorized access"}), 403
+    if request.method in ["PUT", "DELETE"]:
+        if current_user.is_authenticated and clip.user_id == current_user.id:
+            pass
+        else:
+            return jsonify({"error": "Unauthorized access"}), 403
 
-    # if request.method == "PUT":
-    #     form = CreateAlbumForm(obj=album)
+    if request.method == "PUT":
+        form = ClipForm(obj=clip)
 
-    #     form['csrf_token'].data = request.cookies['csrf_token']
-    #     if form.validate_on_submit():
-    #         album.name = form.name.data
-    #         album.releaseDate = form.releaseDate.data
-    #         album.albumType = form.albumType.data
-    #         album.genre = form.genre.data
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            clip.location = form.location.data
+            clip.caption = form.caption.data
+            clip.is_private = form.is_private.data
+            clip.video_file = form.file.data
 
-    #         newImageUrl = form.imageUrl.data
-    #         newImageUrl.filename = get_unique_filename(newImageUrl.filename)
-    #         upload = upload_file_to_s3(newImageUrl)
-    #         print(upload)
+            # newImageUrl = form.imageUrl.data
+            # newImageUrl.filename = get_unique_filename(newImageUrl.filename)
+            # upload = upload_file_to_s3(newImageUrl)
+            # print(upload)
 
-    #         if "url" not in upload:
-    #         # if the dictionary doesn't have a url key
-    #             return render_template("create_track.html", form=form, errors=[upload])
-    #         else:
-    #             # album.image_url = create_presigned_url(newImageUrl.filename, expiration_seconds=157680000)
-    #             album.image_url = upload["url"]
+            # if "url" not in upload:
+            # # if the dictionary doesn't have a url key
+            #     return render_template("create_track.html", form=form, errors=[upload])
+            # else:
+                # album.image_url = upload["url"]
             
-    #         db.session.commit()
-    #         return jsonify({"message": "Album has been updated successfully"}), 201
-    #     else:
-    #         error_messages = {}
-    #         for field, errors in form.errors.items():
-    #             error_messages[field] = errors[0]
+            db.session.commit()
+            return jsonify({"message": "Clip has been updated successfully"}), 201
+        else:
+            error_messages = {}
+            for field, errors in form.errors.items():
+                error_messages[field] = errors[0]
 
-    #         response = jsonify({
-    #             "message": "Bad Request",
-    #             "error": error_messages,
-    #         })
-    #         response.status_code = 400
-    #         return response
+            response = jsonify({
+                "message": "Bad Request",
+                "error": error_messages,
+            })
+            response.status_code = 400
+            return response
 
     if request.method == 'DELETE':
         # remove_file_from_s3(album.image_url)
@@ -163,7 +161,6 @@ def create_clip():
                         user_id = current_user.id)
         db.session.add(new_clip)
         db.session.commit()
-        # album = Album.query.get(new_album.id).to_dict()
         return jsonify({"message": "Clip successfully created."}), 201
 
     print(form.errors)

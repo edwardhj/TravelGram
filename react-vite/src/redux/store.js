f375875354de676ef88cc53bf266c464f7283734
Edward Jung <edwardjung2018@gmail.com>
2024-06-08 00:10:54 -0700

    added clips reducer to store

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import clipsReducer from "./clips";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  clips: clipsReducer,
 });
 
 let enhancer;
